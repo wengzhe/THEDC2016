3f77f2e13a8e21c983a99ef652e6585ee29f49f8
翁喆 <zhe_weng@163.com>
2017-04-08 08:02:54 +0800

    统计脚本基本完善~

--- calculate.sh
+++ calculate.sh
@@ -1,14 +1,19 @@
-extensions=(
+ext=(
 '*.c'
+'*.cpp'
 '*.h'
+'*.py'
+'*.sh'
 )
 
 lines=0;
 lines_useful=0;
-for data in ${extensions[@]}
+for i in ${!ext[@]}
 do
-    let lines=lines+`find . -name "${data}" -exec grep -n "" {} \; | wc -l`
-    let lines_useful=lines_useful+`find . -name "${data}" -exec grep -v ^$ {} \; | wc -l`
+    echo "${ext[$i]}"
+    echo `find . -name "${ext[$i]}"`
+    let lines=lines+`find . -name "${ext[$i]}" -exec cat {} \; | wc -l`
+    let lines_useful=lines_useful+`find . -name "${ext[$i]}" -exec awk -F# '$0' {} \; | wc -l`
 done
 echo "总行数$lines"
 echo "非空行$lines_useful"
@@ -17,6 +22,9 @@ read -p "Press enter to continue..." INP
 
 # exclude blank lines
 # find . -name "${data}"|xargs cat|grep -v ^$|wc -l
+
+# exclude blank lines and begin with #
+# find . -name "${ext[$i]}" -exec awk -F# '$1{print $1}' {} \; | wc -l
 
 # exclude the lines begin with //
 # find . -name "*.java"|xargs cat|grep -v -e ^$ -e ^\s*\/\/.*$|wc -l    #Output:36064
